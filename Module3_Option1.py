a20a9187e5ed66b73e6bcd067ecb6214a1dd4eff
Ivy Martin <144750634+Ivym03@users.noreply.github.com>
2023-12-03 20:07:58 -0500

    Add files via upload

---
+++ Module3_Option1.py
@@ -0,0 +1,35 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Python program to perform basic mathematical operations
+>>> 
+>>> # Function to add two numbers
+>>> def add(x, y):
+...     return x + y
+... 
+>>> # Function to subtract two numbers
+>>> def subtract(x, y):
+...     return x - y
+... 
+>>> # Function to multiply two numbers
+>>> def multiply(x, y):
+...     return x * y
+... 
+>>> # Function to divide two numbers
+>>> def divide(x, y):
+...     return x / y
+... 
+>>> # Take user input for two numbers
+>>> num1 = float(input("Enter first number: "))
+Enter first number: 584
+>>> num2 = float(input("Enter second number: "))
+Enter second number: 2
+>>> 
+>>> # Perform operations and print the results
+>>> print("Sum:", add(num1, num2))
+Sum: 586.0
+>>> print("Difference:", subtract(num1, num2))
+Difference: 582.0
+>>> print("Product:", multiply(num1, num2))
+Product: 1168.0
+>>> print("Quotient:", divide(num1, num2))
+Quotient: 292.0
